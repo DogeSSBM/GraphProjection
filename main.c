19cd58ec23fdeffc6bf1f2ccdcec5c7578e49b28
DogeSSBM <dogessbm@gmail.com>
2021-03-15 03:25:51 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,64 +1,36 @@
 #include "Includes.h"
 
-Coord move(Coord pos, const Length window)
-{
-	const bool dirs[4] = {
-		keyState(SDL_SCANCODE_W) || keyState(SDL_SCANCODE_UP),
-		keyState(SDL_SCANCODE_D) || keyState(SDL_SCANCODE_RIGHT),
-		keyState(SDL_SCANCODE_S) || keyState(SDL_SCANCODE_DOWN),
-		keyState(SDL_SCANCODE_A) || keyState(SDL_SCANCODE_LEFT)
-	};
-	for(Direction i = DIR_U; i <= DIR_L; i++)
-		if(dirs[i] && !dirs[dirINV(i)])
-				pos = coordShift(pos,i,4);
-	return coordWrap(pos, (Range){0,window.x}, (Range){0,window.y});
-}
+typedef struct Node{
+	uint lastvisit;
+	Coord pos;
+	uint numadj;
+	struct Node* *adj;
+}Node;
 
-void trackMouse(const Color color)
-{
-	setColor(u32ToColor(~colorToU32(color)));
-	drawLineCoords(mouse.pos, coordOffset(mouse.pos, coordMul(mouse.vec, 3)));
-	setColor(WHITE);
-	fillCircleCoord(mouse.pos, 3);
-}
-
-void showText(const Coord pos, const Color color, const Coord window, const char *text)
-{
-	setFontColor(color);
-	drawTextCenteredCoord(pos, text);
-	for(Direction i = DIR_U; i <= DIR_L; i++){
-		drawTextCenteredCoord(coordShift(pos, i, dirLR(i)?window.x:window.y), text);
-	}
-
-}
+// have a partial traversal
+// have a global toggle reset
+// maybe have master list
+// for more complex pattern finding keep a traversal history of N nodes
 
 int main(int argc, char const *argv[])
 {
 	const Length window = {800, 600};
 	init(window);
-	setFontSize(48);
-	const char* text = "DogeLib :3";
-	Coord pos = coordDiv(window, 2);
+	printf("%u\n",(uint)sizeof(int));
+	Node* nodes = malloc(sizeof(Node)*3);
+	memset(nodes, 0, sizeof(Node)*3);
+	for(uint i = 0; i < 3; i++){
+		nodes[i].numadj = 2;
+		nodes[i].adj = malloc(sizeof(Node*)*2);
+		nodes[i].adj[0] = &(nodes[wrap(i-1,0,3)]);
+		nodes[i].adj[1] = &(nodes[wrap(i+1,0,3)]);
+	}
 
 	while(1){
 		Ticks frameStart = getTicks();
 		clear();
 
-		setColor(mouseBtnState(MOUSE_L)?MAGENTA:CYAN);
-		drawCircleCoord(mouse.pos, 10);
 
-		setColor(mouseBtnState(MOUSE_L)?CYAN:MAGENTA);
-		Coord vx[8] = {0};
-		for(uint i = 0; i < 4; i++){
-			vx[i*2] = coordShift(mouse.pos, i, 80);
-			vx[i*2+1] = coordShift(coordShift(mouse.pos, dirINV(i), 60), dirROR(dirINV(i)), 60);
-		}
-		drawPoly(vx, 8);
-
-		const Color color = mouseBtnState(MOUSE_L)?BLUE:RED;
-		pos = move(pos, window);
-		trackMouse(color);
-		showText(pos, color, window, text);
 
 		draw();
 		events(frameStart + TPF);
